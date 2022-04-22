dbf42d9f32a603c79eaa54d8c9078f98026558a6
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-04-21 21:49:36 -0600

    Add sample data

---
+++ test/sample-bookings.js
@@ -0,0 +1,33 @@
+const sampleBookings = [
+  {
+  id: "5fwrgu4i7k55hl6sz",
+  userID: 1,
+  date: "2022/04/22",
+  roomNumber: 1
+  },
+  {
+  id: "5fwrgu4i7k55hl6t5",
+  userID: 1,
+  date: "2022/01/24",
+  roomNumber: 2
+  },
+  {
+  id: "5fwrgu4i7k55hl6t6",
+  userID: 2,
+  date: "2022/01/10",
+  roomNumber: 3
+  },
+  {
+  id: "5fwrgu4i7k55hl6t7",
+  userID: 3,
+  date: "2022/02/16",
+  roomNumber: 4
+  },
+  {
+  id: "5fwrgu4i7k55hl6t8",
+  userID: 3,
+  date: "2022/02/05",
+  roomNumber: 4
+  }
+];
+export default sampleBookings;
