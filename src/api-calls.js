5d8f25f5f0fb20737fe8575da52288704a134560
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-05-27 10:21:18 -0600

    Update fetch

--- src/api-calls.js
+++ src/api-calls.js
@@ -1,5 +1,5 @@
 const getData = (dataLocation) => {
-  return fetch(`http://localhost:3001/api/v1/${dataLocation}`)
+  return fetch(`https://pacific-plateau-49208.herokuapp.com/api/v1/${dataLocation}`)
  .then(response => {
    if (!response.ok) {
      throw Error(response.statusText)
@@ -10,7 +10,7 @@ const getData = (dataLocation) => {
 }
 
 const postData = (userID, date, roomNum) => {
-  return fetch("http://localhost:3001/api/v1/bookings", {
+  return fetch("https://pacific-plateau-49208.herokuapp.com/api/v1/bookings", {
     method: 'POST',
     body: JSON.stringify({
       userID: userID,
